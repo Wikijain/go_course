a880d89d79177d8084e195dfbddd3616326b485f
Harsh Jain <harsh.jain@netcracker.com>
2024-07-10 12:49:20 +0530

    csv read and write

--- UATGoCode/main.go
+++ UATGoCode/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strings"
+
 )
 
 // readPostgreSQLCSV reads a PostgreSQL CSV file and returns a list of keys.
@@ -79,28 +81,88 @@ func main() {
 
 	postgresqlKeys := readPostgreSQLCSV(postgresqlCSV)
 	cassandraData := readCassandraCSV(cassandraCSV)
+	// Create or open the CSV file
+	file, err := os.Create("output_log.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create a CSV writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush() // Ensure all data is written
+
+	// Write the header row (optional)
+	header := []string{"errorCode", "errorMsg"}
+	writer.Write(header)
+
+	// create data rows
+	data := [][]string{}
 
 	for _, postgresqlKey := range postgresqlKeys {
 		postgresqlID := postgresqlKey[0]
 		postgresqlColumn2 := postgresqlKey[1]
 
-		matchFound := false
-
+		matchFoundID := false
+		matchFoundCategory := false
 		for _, cassandraRecord := range cassandraData {
 			jsonData := cassandraRecord[0].(string)
-			//arrayData := cassandraRecord[1].([]interface{})
+			arrayData := cassandraRecord[1].([]interface{})
 			if strings.Contains(jsonData, postgresqlID) {
-				//log.Printf("Found '%s' \n", postgresqlID)
-				matchFound = true
-				break
+				matchFoundID = true
+			}
+			var cassandraJSON map[string]interface{}
+			err := json.Unmarshal([]byte(jsonData), &cassandraJSON)
+			if err != nil {
+				log.Printf("Failed to parse JSON data: %s", jsonData)
+				continue
+			}
+			cassandraID, ok := cassandraJSON["id"].(string)
+			if !ok {
+				log.Printf("Invalid ID format in Cassandra record: %v", cassandraJSON)
+				continue
+			}
+			if postgresqlID == cassandraID {
+				if arrayContainsString(arrayData, postgresqlColumn2) {
+					matchFoundCategory = true
+				}
 			}
 		}
 
-		if !matchFound {
-			log.Printf("No match found for PostgreSQL id '%s', '%s'", postgresqlID, postgresqlColumn2)
+		if !matchFoundID && !matchFoundCategory {
+			log.Printf("No match found for PostgreSQL id '%s',and Category '%s'", postgresqlID, postgresqlColumn2)
+			errorMsg := fmt.Sprintf("No match found for PostgreSQL id '%s',and Category '%s'", postgresqlID, postgresqlColumn2)
+			myslice := []string{"3", errorMsg}
+			data = append(data, myslice)
+		} else if !matchFoundID {
+			log.Printf("No match found for PostgreSQL id '%s'", postgresqlID)
+			errorMsg := fmt.Sprintf("No match found for PostgreSQL id '%s'", postgresqlID)
+			myslice := []string{"1", errorMsg}
+			data = append(data, myslice)
+		} else if !matchFoundCategory {
+			log.Printf("No match found for  Category '%s'", postgresqlColumn2)
+			errorMsg := fmt.Sprintf("No match found for  Category '%s'", postgresqlColumn2)
+			myslice := []string{"2", errorMsg}
+			data = append(data, myslice)
 		}
 	}
 
 	log.Println("Data comparison process completed.")
+	for _, row := range data {
+		err := writer.Write(row)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
 
+// arrayContainsString checks if a string is contained in an array of interfaces.
+func arrayContainsString(array []interface{}, str string) bool {
+	for _, elem := range array {
+		s := fmt.Sprintf("%v", elem)
+		if strings.Contains(s, str) {
+			return true
+		}
+	}
+	return false
 }
